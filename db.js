c0335b7242386e21829e1e3c64e1e0aa5edc66e6
Glitch (tinyap) <none>
2019-04-12 17:19:24 +0000

    💎⛩ Checkpoint ./server.js:809753/1656 ./db.js:809753/1147

---
+++ db.js
@@ -0,0 +1,31 @@
+// init sqlite db
+const fs      = require('fs');
+const sqlite3 = require('sqlite3').verbose();
+
+const dbFile  = './.data/sqlite.db';
+const db      = new sqlite3.Database(dbFile);
+
+const exists  = fs.existsSync(dbFile);
+
+// if ./.data/sqlite.db does not exist, create it, otherwise print records to console
+db.serialize(function(){
+  if (!exists) {
+    db.run('CREATE TABLE Dreams (dream TEXT)');
+    console.log('New table Dreams created!');
+    
+    // insert default dreams
+    db.serialize(function() {
+      db.run('INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")');
+    });
+  }
+  else {
+    console.log('Database "Dreams" ready to go!');
+    db.each('SELECT * from Dreams', function(err, row) {
+      if ( row ) {
+        console.log('record:', row);
+      }
+    });
+  }
+});
+
+module.exports = exports = db;
--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 const express = require('express');
 const app = express();
 
+
+
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
